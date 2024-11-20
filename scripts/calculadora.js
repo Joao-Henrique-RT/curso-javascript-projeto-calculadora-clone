8193ae46d98a6f964b857b6df75822ec1172dd0b
João Henrique Ribeiro Teixeira <86114318+Joao-Henrique-RT@users.noreply.github.com>
2024-11-20 18:30:43 -0300

    GET E SET

--- scripts/calculadora.js
+++ scripts/calculadora.js
@@ -1,2 +1,5 @@
-let calculadora = new CalcController
+let calculadora = new CalcController()
 //Nesta declaração dizemos que a variavel calculadora é uma cópis da classe CalcController
+
+
+console.log(calculadora._dataAtual)
--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,10 +1,43 @@
 class CalcController{
 
+    
+
+    constructor(){
+        //palavra reservada THIS para referenciar atributos e objetos
+         this._displayCalc = "0";
+         this._dataAtual;
+    }
+
+    get displayCalc(){
+        //GETTER - Pegar o valor do atributo displayCalc
+        return this.displayCalc;
+    }
+
+    set displayCalc(valor){
+        //SETTER - Colocar um valor dentro do atributo display calc. Precisa necessáriamente de um parametro (valor) para ser atribuido 
+        this._displayCalc = valor ;
+
+    }
+
+    get dataAtual(){
+        return this._dataAtual;
+    }
+
+    set dataAtual(data){
+        this._dataAtual = data
+    }
+
+
+
+
 
 
 }
 
-/*  -Dentro de uma classe deveremos ter variaveis e funções (atributos e metodos)
+
+/*  -Todas as regras de negócio deverão estar dentro desta classe
+
+    -Dentro de uma classe deveremos ter variaveis e funções (atributos e metodos)
 
     -Objeto representa a classe, por meio dele que chamamos a classe
 */
