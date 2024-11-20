368df8450663909d78b2647167f3c3eb047bfdc2
João Henrique Ribeiro Teixeira <86114318+Joao-Henrique-RT@users.noreply.github.com>
2024-11-20 17:49:30 -0300

    Inicio do projeto
    Criação de arquivos JS para padrão MVC

---
+++ scripts/calculadora.js
@@ -0,0 +1,2 @@
+let calculadora = new CalcController
+//Nesta declaração dizemos que a variavel calculadora é uma cópis da classe CalcController
---
+++ scripts/controller/calcController.js
@@ -0,0 +1,10 @@
+class CalcController{
+
+
+
+}
+
+/*  -Dentro de uma classe deveremos ter variaveis e funções (atributos e metodos)
+
+    -Objeto representa a classe, por meio dele que chamamos a classe
+*/
